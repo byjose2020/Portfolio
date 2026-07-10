ae610fc879b15db8b8cc34a6b36779d387e2d1cc
Jose Chapellin <byjose2020@gmail.com>
2026-07-10 12:15:51 -0400

    Add files via upload

---
+++ UltimateTS/index.ts
@@ -0,0 +1,13 @@
+// cualquiertipo de dato 
+// con en any le decimos que ignore cualquiertipado de typescript
+// let anyValue: any = "hola mundo"
+
+// anyValue.toUpperCase()
+
+// let anyValue2: unknown = 'hola'
+
+// anyValue2.aa
+
+const persona = {name: 'jose', age: 25}
+
+persona.name
